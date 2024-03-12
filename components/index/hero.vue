82ca0bb0917f09f64f1fa7c1eb0e4dd189ed9644
huangbh <578092215@qq.com>
2024-03-12 13:42:13 +0800

    feat(pages): :sparkles: 完成首页hero样式编写

---
+++ components/index/hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
+      <div class="px-6">
+        <h1
+          class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl my-5"
+        >
+          欢迎访问我的博客
+        </h1>
+        <p class="dark:text-zinc-300">
+          这边后续换成一言，实现文字打印效果
+        </p>
+      </div>
+      <div class="px-6 justify-self-center">
+        <LogoDog />
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>Index Page</div>
+  <main class="container max-w-5xl mx-auto text-zinc-600">
+    <IndexHero />
+  </main>
 </template>
