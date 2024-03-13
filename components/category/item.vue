b700502cb6f5693ded3a9296260d2ed296d68005
huangbh <578092215@qq.com>
2024-03-13 11:32:08 +0800

    feat(pages): :sparkles: 完成categories页面样式编写

---
+++ components/category/item.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+defineProps<{ title: string, count: number }>()
+const colors = [
+  '#dc2626',
+  '#d97706',
+  '#65a30d',
+  '#059669',
+  '#0891b2',
+  '#0284c7',
+  '#4f46e5',
+  '#7c3aed',
+  '#c026d3',
+  '#db2777',
+]
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+const picAColor = ref(`${colors.at(getRandomInt(0, colors.length - 1))}`)
+</script>
+
+<template>
+  <div
+    class="text-[#F1F2F4] px-5 py-3 rounded hover:underline rand-bg-color hover:scale-[1.05] transition-all duration-500"
+  >
+    <NuxtLink class="text-lg font-extrabold">
+      <h1>#{{ title }}({{ count }})</h1>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.rand-bg-color {
+  background-color: v-bind(picAColor);
+}
+</style>
--- pages/categories.vue
+++ pages/categories.vue
@@ -1,3 +1,17 @@
+<script lang="ts" setup>
+import type { CategoryItem } from '@/types/category'
+
+const categories: CategoryItem[] = [
+  { id: 1, title: '前端', count: 2 },
+  { id: 2, title: 'JavaScript', count: 1 },
+]
+</script>
+
 <template>
-  <div>categories</div>
+  <main class="container max-w-5xl mx-auto text-zinc-600">
+    <CategoryHero />
+    <div class="flex flex-wrap px-6 mt-12 gap-3">
+      <CategoryItem v-for="item in categories" :key="item.id" v-bind="item" />
+    </div>
+  </main>
 </template>
