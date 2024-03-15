6f69e4029112f0b64ad5bf9329c8b329098c7d3f
huangbh <578092215@qq.com>
2024-03-15 17:28:41 +0800

    feat(styles): :sparkles: 滚动条样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width,initial-scale=1',
+      title: '黄某人\'s Blog',
+      meta: [{ name: 'description', content: '黄某人的博客' }],
+    },
   },
   colorMode: {
     preference: 'system',
@@ -11,6 +17,7 @@ export default defineNuxtConfig({
     dataValue: 'theme',
   },
   devtools: { enabled: true },
+  css: ['~/assets/css/scroll.css'],
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
